60f029afda8c53d822f94d1f39c3486ff48a52d9
Rideek <rabinskid@rabdev.pl>
2024-09-13 23:46:13 +0200

    create section heder with logo FORA

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { IntegrationComponentComponent } from './integration-component/integration-component.component';
+import { HeaderComponent } from './header/header.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, IntegrationComponentComponent],
+  imports: [RouterOutlet, IntegrationComponentComponent, HeaderComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.sass',
 })
