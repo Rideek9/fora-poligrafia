3cdfeaf3a57d80664012b8be3e80f902b1bc3f13
Rideek <rabinskid@rabdev.pl>
2024-10-10 20:07:25 +0200

    update mobile wersion to form

---
+++ src/app/private-politic/private-politic.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-private-politic',
+  standalone: true,
+  imports: [],
+  templateUrl: './private-politic.component.html',
+  styleUrl: './private-politic.component.sass'
+})
+export class PrivatePoliticComponent {
+
+}
