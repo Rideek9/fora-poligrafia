ec059704f27b4eb3e94729308c42efadfc3c02fc
Rideek <rabinskid@rabdev.pl>
2024-10-10 21:29:40 +0200

    add privat politic

--- src/app/pages/polityc/polityc.component.ts
+++ src/app/pages/polityc/polityc.component.ts
@@ -1,12 +1,11 @@
 import { Component } from '@angular/core';
+import { PrivatePoliticComponent } from '../../elements/private-politic/private-politic.component';
 
 @Component({
   selector: 'app-polityc',
   standalone: true,
-  imports: [],
+  imports: [PrivatePoliticComponent],
   templateUrl: './polityc.component.html',
-  styleUrl: './polityc.component.sass'
+  styleUrl: './polityc.component.sass',
 })
-export class PolitycComponent {
-
-}
+export class PolitycComponent {}
